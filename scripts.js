77ac6b22733806d558139f3e751c4ec64f55b756
Raphael <Rapheal.yeom@gmail.com>
2024-02-14 20:50:27 -0500

    carousel fixed

--- scripts.js
+++ scripts.js
@@ -3,20 +3,22 @@ if(window.matchMedia("(min-width:576px)").matches){
     const carousel = new bootstrap.Carousel(multipleItemCarousel, {
         interval: false
     });
-    var carouselWidth = $('.carousel-inner').scrollWidth;
+
+    var carouselWidth = $('.carousel-inner')[0].scrollWidth;
     var cardWidth = $('.carousel-item').width();
     var scrollPosition = 0;
-    
     $('.carousel-control-next').on('click', function(){
-        if(scrollPosition < (carouselWidth - (cardWidth * 4 ))){
+        if(scrollPosition < (carouselWidth - (cardWidth * 3))){
+            console.log('next');
             scrollPosition = scrollPosition + cardWidth;
-            $('.carousel-inner').animate({scrollLeft: scrollPoisition},600); 
+            $('.carousel-inner').animate({scrollLeft: scrollPosition},600); 
         }
     });
     $('.carousel-control-prev').on('click', function(){
         if(scrollPosition > 0){
+            console.log('prev');
             scrollPosition = scrollPosition - cardWidth;
-            $('.carousel-inner').animate({scrollLeft: scrollPoisition},600); 
+            $('.carousel-inner').animate({scrollLeft: scrollPosition},600);
         }
     });
 } else{
